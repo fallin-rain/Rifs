06ab9822d3a9149c27ddc492c9673e0d17ca6525
asweetrain <rishav.m.19.02@gmail.com>
2022-06-05 18:42:40 +0530

    Change optional params

--- src/lib/utils/lazyload.ts
+++ src/lib/utils/lazyload.ts
@@ -1,5 +1,9 @@
-export function lazyload(selector, params) {
+export function lazyload(selector: string, params) {
 	if (typeof IntersectionObserver === 'undefined') return
+
+	params.autoplay = params.autoplay || false
+	params.margin = params.margin || '0px'
+	params.threshold = params.threshold || 0
 
 	const observer = new IntersectionObserver(
 		entries => {
@@ -10,10 +14,7 @@ export function lazyload(selector, params) {
 					video.src = video.dataset.src
 					video.poster = video.dataset.poster
 
-					// video.removeAttribute('data-src')
-					// video.removeAttribute('data-poster')
-
-					// video.load()
+					if (params.autoplay) video.play()
 
 					observer.unobserve(video)
 				}
@@ -24,8 +25,6 @@ export function lazyload(selector, params) {
 			threshold: params.threshold,
 		}
 	)
-
-	// document.querySelectorAll(selector).forEach(card => observer.observe(card))
 
 	const newObserver = new IntersectionObserver(
 		entries => {

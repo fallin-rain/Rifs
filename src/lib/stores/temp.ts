34bd0723979e6c4113acb0ebfa9263f79d79ccbe
asweetrain <rishav.m.19.02@gmail.com>
2022-06-09 15:34:13 +0530

    No need store

--- src/lib/stores/temp.ts
+++
@@ -1,6 +0,0 @@
-import { browser } from '$app/env'
-import { writable } from 'svelte/store'
-
-export let temp = writable((browser && localStorage.getItem('temp')) || [])
-
-// TODO lets see what can i do with these

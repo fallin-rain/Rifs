cd801f7687d94d4de1ab58f44ae76bfe696f3bad
asweetrain <rishav.m.19.02@gmail.com>
2022-06-17 21:39:15 +0530

    Entry to svg icons and buttons as svelte components

---
+++ src/lib/shared/buttons/Volume__btn.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { createEventDispatcher } from 'svelte'
+	import Volume from '$lib/shared/icons/Volume__ico.svelte'
+	import Muted from '../icons/Muted__ico.svelte'
+
+	export let muted = true
+	export let hint = {
+		onmute: '',
+		onunmute: '',
+	}
+	const dispatch = createEventDispatcher()
+</script>
+
+<button on:click={() => dispatch('click')} class="btn gap-1 {$$restProps.class}">
+	{#if muted}
+		<Muted />
+		{hint.onmute}
+	{:else}
+		<Volume />
+		{hint.onunmute}
+	{/if}
+</button>
---
+++ src/lib/shared/icons/Cross__ico.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let h = '6'
+	export let w = '6'
+	export let text = ''
+	export let bg = ''
+</script>
+
+<div class="h-{h} w-{w} text-{text} bg-{bg} {$$restProps.class}" class:hidden={$$restProps.hidden}>
+	<svg
+		class="h-full w-full"
+		viewBox="0 0 24 24"
+		fill="currentColor"
+		xmlns="http://www.w3.org/2000/svg"
+		stroke="currentColor"
+	>
+		<path
+			d="M8.46447 15.5355L15.5355 8.46446"
+			stroke="currentColor"
+			stroke-width="1.5"
+			stroke-linecap="round"
+		/>
+		<path
+			d="M8.46447 8.46447L15.5355 15.5355"
+			stroke="currentColor"
+			stroke-width="1.5"
+			stroke-linecap="round"
+		/>
+	</svg>
+</div>
